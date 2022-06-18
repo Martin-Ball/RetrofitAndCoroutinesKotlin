b8007a4be43367630ea7e604cbf2633da7726e3e
Martin Ballester <martinballester1@gmail.com>
2022-06-17 22:05:27 -0300

    App work for search breed of dog

--- app/src/main/java/com/martin/retrofitycorrutinas/MainActivity.kt
+++ app/src/main/java/com/martin/retrofitycorrutinas/MainActivity.kt
@@ -14,6 +14,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
 
+
+//TUTORIAL ARISTIDEV https://www.youtube.com/watch?v=aQP-mUGWh1U&list=PL8ie04dqq7_OcBYDpvHrcSFVoggLi3cm_&index=23&ab_channel=Programaci%C3%B3nAndroidbyAristiDevs
+
 class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {  //OnQueryTextListener es el listener del searchview, cuando se escribe devuelve el texto
 
     private lateinit var binding : ActivityMainBinding
@@ -39,23 +42,24 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {  //On
     //esta funcion va a instanciar el objeto retrofit va a tener toda la url, el conversor json a modelo de datos y todas las configs
     private fun getRetrofit():Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://dog.ceo/api/breeds/")
+            .baseUrl("https://dog.ceo/api/breed/")
             .addConverterFactory(GsonConverterFactory.create())     //gson es el conversor de json a la dataclass
             .build()
     }
 
     //creacion de la corrutina, toda la consulta se debe ejecutar en otro hilo que no sea el principal
     private fun searchByName(query:String){
+
         CoroutineScope(Dispatchers.IO).launch{
         //lo que se ejecute en esta seccion sera procesado en otro hilo
-            val call:Response<DogsResponse> = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
-            val puppies:DogsResponse? = call.body()     //el signo de pregunta verifica si es null
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val puppies = call.body()     //el signo de pregunta verifica si es null
 
             runOnUiThread{
                 //lo que se ejecute aqui sera ejecutado en el hilo principal
                 if(call.isSuccessful){
                     //mostrar recyclerView
-                    val images:List<String> = puppies?.images ?: emptyList() //el simbolo ?: verifica si es nulo o no, en caso de que lo sea devuelve emptyList
+                    val images = puppies?.images ?: emptyList() //el simbolo ?: verifica si es nulo o no, en caso de que lo sea devuelve emptyList
                     dogImages.clear()
                     dogImages.addAll(images)
                     adapter.notifyDataSetChanged()
@@ -74,7 +78,7 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {  //On
     override fun onQueryTextSubmit(query: String?): Boolean {
         //cuando se toca en buscar
         if(!query.isNullOrEmpty()){
-            searchByName(query.toLowerCase())
+            searchByName(query.lowercase())
         }
         return true
     }
